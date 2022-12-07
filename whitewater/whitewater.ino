47ac88461b75efd8292d2f1ba5e0322f5ce4916d
William H. Grover <wgrover@gmail.com>
2022-12-06 21:10:29 -0800

    added activity LED

--- whitewater/whitewater.ino
+++ whitewater/whitewater.ino
@@ -20,46 +20,48 @@ const int resolution = 8;
 WiFiClientSecure client;
 
 void setup() {
-   Serial.begin(115200);
-   WiFi.mode(WIFI_STA); 
-   WiFi.begin(ssid, password);
-   while (WiFi.status() != WL_CONNECTED) {
-      delay(500);
-      Serial.print(".");
-   }
-   Serial.println("");
-   Serial.print("WiFi connected to: ");
-   Serial.println(ssid);
-   Serial.println("IP address: ");
-   Serial.println(WiFi.localIP());
-   delay(2000);
-   ledcSetup(ledChannel, freq, resolution);
-   ledcAttachPin(ledPin, ledChannel);
+  Serial.begin(115200);
+  WiFi.mode(WIFI_STA);
+  WiFi.begin(ssid, password);
+  while (WiFi.status() != WL_CONNECTED) {
+    delay(500);
+    Serial.print(".");
+  }
+  Serial.println("");
+  Serial.print("WiFi connected to: ");
+  Serial.println(ssid);
+  Serial.println("IP address: ");
+  Serial.println(WiFi.localIP());
+  delay(2000);
+  ledcSetup(ledChannel, freq, resolution);
+  ledcAttachPin(ledPin, ledChannel);
+  pinMode(2, OUTPUT);
 }
 
 void loop() {
+  digitalWrite(2, HIGH);
   if ((WiFi.status() == WL_CONNECTED)) {
- 
+
     HTTPClient http;
- 
-    http.begin("https://waterservices.usgs.gov/nwis/iv/?format=json&sites=10257548"); 
+
+    http.begin("https://waterservices.usgs.gov/nwis/iv/?format=json&sites=10257548");
     int httpCode = http.GET();
     if (httpCode > 0) {
-        String payload = http.getString();
-        Serial.println(httpCode);
-        // Serial.println(payload);
-        DynamicJsonDocument doc(10000);
-        deserializeJson(doc, payload);
-        double sensor = doc["value"]["timeSeries"][0]["values"][0]["value"][0]["value"];
-        int pwm = 255.0 * sensor/200.0;
-        Serial.println(pwm);
-        ledcWrite(ledChannel, pwm); 
-      }
+      String payload = http.getString();
+      Serial.println(httpCode);
+      // Serial.println(payload);
+      DynamicJsonDocument doc(10000);
+      deserializeJson(doc, payload);
+      double sensor = doc["value"]["timeSeries"][0]["values"][0]["value"][0]["value"];
+      int pwm = 255.0 * sensor / 200.0;
+      Serial.println(pwm);
+      ledcWrite(ledChannel, pwm);
+    }
     else {
       Serial.println("Error on HTTP request");
     }
     http.end();
   }
- 
+  digitalWrite(2, LOW);
   delay(10000);
 }
